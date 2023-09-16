8bd1156a49f1b09682dcd1650a4dd14b6185d716
Jeetu Gupta <jeetu270602@gmail.com>
2023-09-16 13:46:35 +0530

    Add functionality to input boxes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,22 @@
+import {useState} from 'react'
+
 export default function Main(){
-    
+    const [meme, setMeme] = useState({
+        upperText : "",
+        bottomText : "",
+        image : "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function updateMeme(event){
+        const {name, value} = event.target
+        setMeme(prevText => {
+            return {
+                ...prevText,
+                [name] : value
+            }
+        })
+    }
+
     return (
         <main>
             <div id = "input-container">
@@ -7,11 +24,13 @@ export default function Main(){
                     type="text"
                     name = "upperText"
                     placeholder="Upper Text"
+                    onChange = {updateMeme}
                 />
                 <input
                     type="text"
                     name = "bottomText"
                     placeholder="Bottom Text"
+                    onChange = {updateMeme}
                 />
             </div>
             <button
@@ -21,8 +40,8 @@ export default function Main(){
             </button>
             <div id = "meme">
                 <img src = "http://i.imgflip.com/1bij.jpg" alt = "meme image" id = "meme-image"/>
-                <h1 id = "upperText" className = "top">Upper Text</h1>
-                <h1 id = "bottomText" className = "bottom">Bottom Text</h1>
+                <h1 id = "upperText" className = "top">{meme.upperText}</h1>
+                <h1 id = "bottomText" className = "bottom">{meme.bottomText}</h1>
             </div>
         </main>
     )
